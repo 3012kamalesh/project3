2d11b35748a413060d29baf47514721f22fe0c4d
3012kamalesh <143986667+3012kamalesh@users.noreply.github.com>
2023-11-02 23:30:46 -0700

    Update pending.py

--- pending.py
+++ pending.py
@@ -11,39 +11,6 @@
 ##    print(f"{x}-{y}")
 ##
 ##        
-  
-##next permutation:
-##nums=[1,2,3]
-##nums.remove(3)
-##print(nums)
-##nums.insert(1,3)
-##print(nums)
-
-## project:BMI calucator 
-##weight=float(input("enter the  weight in kg"))
-##height=float(input("enter the height in meter"))
-##BMI=weight/height**2
-##print(BMI)
-##if BMI< 18.25:
-##    print("under weight")
-##elif BMI< 50:
-##    print("healthy weight")
-##else:
-##    print("over weight")
-
-
-##project:quiz game
-
-player=input("do you want to play the game")
-if player !="yes":
-    quit()
-print("ok lets play")
-score=0
-question=input("1. Who developed Python Programming Language?")
-if question.lower()=="Guido van Rossum":
-    print("correct")
-    score+=1
-else:
-    print("incorrect")
+  
 
     
